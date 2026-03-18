46b0adf20aef6d0a6730aa3591da31d648947e08
Iceraph <r.wothkedusseaux@gmail.com>
2026-03-18 19:49:39 +0100

    fix: try SUPER_PLUS first

--- camera/camera/cameras/oakd_stereo_camera.py
+++ camera/camera/cameras/oakd_stereo_camera.py
@@ -160,7 +160,7 @@ class OakDStereoCamera:
         last_err = None
 
         # Prefer SUPER (USB3) for stability; try SUPER_PLUS as a second attempt.
-        for speed in (dai.UsbSpeed.SUPER, dai.UsbSpeed.SUPER_PLUS, dai.UsbSpeed.HIGH):
+        for speed in (dai.UsbSpeed.SUPER_PLUS, dai.UsbSpeed.SUPER, dai.UsbSpeed.HIGH):
             try:
                 dev = dai.Device(self.pipeline, maxUsbSpeed=speed)
 
